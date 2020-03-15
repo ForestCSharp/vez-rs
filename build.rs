812491655a0268ba320446de4a86264d8a7a4473
Forest Sharp <forestcollinssharp@gmail.com>
2020-03-15 15:15:58 -0500

    update submodule V-EZ

--- build.rs
+++ build.rs
@@ -5,10 +5,9 @@ use std::env;
 //FIXME: complaining about missing struct tags in bindgen (even though they are typedef'd) (had to be manually changed)
 //TODO: test above in a normal c project, could be a V-EZ issue
 
-//TODO: fork V-EZ so we can fix cmake output dir to be in our build location? 
+//TODO: fork V-EZ so we can fix cmake output dir to be in our build location?
 
 fn main() {
-
     let out_dir = env::var("OUT_DIR").expect("failed to get OUT_DIR");
 
     let vk_dir = env::var("VULKAN_SDK").expect("failed to find Vulkan SDK");
@@ -28,9 +27,7 @@ fn main() {
 
     println!("OUT DIR: {}", out_dir);
 
-    let _dst = cmake::Config::new("V-EZ")
-        .define("VEZ_OUTPUT_DIRECTORY", &out_dir)
-        .build();
+    let _dst = cmake::Config::new("V-EZ").build();
 
     println!("cargo:rustc-link-search={}\\Lib", vk_dir);
     println!("cargo:rustc-link-lib=vulkan-1");
